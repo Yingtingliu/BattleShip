0b08af84ec6828aff5d6f88d5ab519012f4f1384
Yingtingliu <yi999897@gmail.com>
2022-11-02 23:28:22 +0000

    initial

---
+++ BattleShip/src/BattleShip/BattleShip.java
@@ -0,0 +1,13 @@
+package BattleField;
+
+public class BattleShip {
+	
+	boolean sunk; //True for sunk
+	Integer health; //remaining health
+	Integer size; //size of the ship
+	
+	
+	
+//	Square[] s1 = new Square[2];
+
+}
