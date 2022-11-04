65638e86233a0a592447fcbf95ffb79965822b46
wl02338983 <wl02968970@gmail.com>
2022-11-04 11:25:57 +0800

    11/4測試

--- BattleShip/src/BattleShip/text1.java
+++ BattleShip/src/BattleShip/text1.java
@@ -8,10 +8,22 @@ public class text1 {
 		int row = 10;
 		int column = 10;
 		text2 b1 = new text2(row,column);
-		b1.toString(b1);
+		text2 b2 = new text2(row,column);
+		text3[][] gameBoard1 = b1.gameBord(row, column);
+		b1.toString(b1,gameBoard1);
+		b1.toStringViewShip(b1,gameBoard1);
+//		b2.toString(b2);
+//		b2.toStringViewShip(b2)	;
 		
-		text4 b2 = new text4(null, null);
-		b2.takeTurn(b2, null);
+		boolean turn = false;		
+		text4 p1 = new text4(b1, "Player1");
+		text4 p2 = new text4(b2, "Player2");
+		while(!turn ) {			
+			turn = p1.takeTurn(b1,p1, gameBoard1);
+			if(turn) {break;}
+			turn = p2.takeTurn(b2, p2, null);
+		}		
+		p1.gameOver(p1,p2);
 	}
 
 }
--- BattleShip/src/BattleShip/text4.java
+++ BattleShip/src/BattleShip/text4.java
@@ -3,42 +3,76 @@ package BattleShip;
 import java.util.Scanner;
 
 public class text4 {
-	text3 board;
+	text2 board;
 	String name;
 	int score;	
 	
 	//constructor
-	public text4(text3 board, String name) {
+	public text4(text2 board, String name) {
 		this.board = board;
 		this.name = name;		
 	}
 	
-	public boolean takeTurn(text4 text4, Square[][] gameboard) {
+	public boolean takeTurn(text2 board,text4 player, text3[][] gameboard) {
 		String temp, firstWord, secondWord;
+		System.out.println("Please enter the position x y from 0-9, eg: 0 1");
 		Scanner in = new Scanner(System.in);
-		boolean attack = false;
-		while(!attack) {
 		//printout player's input 
 		temp = in.nextLine();
 		System.out.println(temp);
 		
 		in = new Scanner(temp); // make it a scanner
-		firstWord = in.next(); // get the first word
+		firstWord = in.next(); 
 		secondWord = in.next();
 		int row = Integer.valueOf(firstWord);
-		int column = Integer.valueOf(secondWord);
+		int column = Integer.valueOf(secondWord);		
 		
-		gameboard[row][column].setShipCurrentStatus(true);
+		// hit the square		
+		if(gameboard[row][column].isFire()) {
+			//if there's ship
+			gameboard[row][column].setShipCurrentStatus(true);
+			gameboard[row][column].setFire(true);
+		} else {
+			//there's no ship
+			gameboard[row][column].setFire(true);
+		}		
+		
+		if(!hitAllShips()) {			
+			return false;
+		}else {
+			//fired all the ships
+			return true;
+		}	
+				
+	}
+	
+	public boolean hitAllShips() {
+		
+		
+		
+		return false;
+		
+	}
+	
+	public void gameOver(text4 p1, text4 p2) {
+		System.out.println("Your ships: " + p1.name + " | Computer ships: " + p2.name);
+		if (p1.hitAllShips()) {
+			System.out.println(p1.name + ",You won the battle! ");
+			System.out.println(p2.name+",You lost the battle! ");
 		}
-		return false;		
+		else if(p2.hitAllShips()) {
+			System.out.println(p2.name + ",You won the battle! ");
+			System.out.println(p1.name+",You lost the battle! ");
+		}			
+		System.out.println();
 	}
 	
 
 	//getter and setter
-	public text3 getBoard() {
+	public text2 getBoard() {
 		return board;
 	}
-	public void setBoard(text3 board) {
+	public void setBoard(text2 board) {
 		this.board = board;
 	}
 	public String getName() {
